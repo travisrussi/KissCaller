1f8a8195ac97dbe99e76c32a1f0100e0097711e0
Travis Russi <github@russigroup.com>
2012-02-01 02:27:16 -0500

    cleaned up references

--- startKissCallerNode.bat
+++ startKissCallerNode.bat
@@ -1,7 +1,6 @@
 @echo off
 set host=kissgen.com
 set port=3000
-set socketIoConsoleExe="C:\MyCode\KissCaller\KissCaller.SocketIo\bin\Debug\KissCaller.SocketIo.exe"
 @echo on
 
 start "node cmd" /B node KissCaller.NodeJs/kissCaller.js --host %host% --port %port%
